80ea0af6dd117148637ce164b6881b89d12d6329
Oleksii Mykhalchuk <ale.m100888@gmail.com>
2022-05-20 16:56:09 +0200

    Initial Commit

---
+++ Dice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dice
+//
+//  Created by Oleksii Mykhalchuk on 5/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
