bbc8a2c76e90a6610dbeb1e708f98380d8a73801
Oleksii Mykhalchuk <ale.m100888@gmail.com>
2022-06-11 18:48:57 +0900

    pod install, .gitignore, rswift, swiftlint

--- Dice/View/ViewController.swift
+++ Dice/View/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-let dice = [
+private let dice = [
   "inverted-dice-1",
   "inverted-dice-2",
   "inverted-dice-3",
@@ -21,64 +21,77 @@ let dice = [
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    image = UIImageView()
-    image.image = UIImage(named: dice[0])
-//    image.bounds.size = CGSize(width: 200, height: 200)
-    view.addSubview(image)
-    image.translatesAutoresizingMaskIntoConstraints = false
-    let constraints = [
-      image.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-      image.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-      image.widthAnchor.constraint(equalToConstant: 200),
-      image.heightAnchor.constraint(equalToConstant: 200)
-    ]
-    NSLayoutConstraint.activate(constraints)
-    button = UIButton()
-    button.setTitle("Roll", for: .normal)
-    button.setTitleColor(.black, for: .highlighted)
-    button.backgroundColor = .red
-    button.layer.cornerRadius = 20
-    view.addSubview(button)
-    button.translatesAutoresizingMaskIntoConstraints = false
-    let constraintsBTN = [
-      button.topAnchor.constraint(equalTo: image.bottomAnchor, constant: 50),
-      button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-      button.widthAnchor.constraint(equalToConstant: 100),
-      button.heightAnchor.constraint(equalToConstant: 50)
-    ]
-    NSLayoutConstraint.activate(constraintsBTN)
-    button.addTarget(self, action: #selector(Roll), for: .touchUpInside)
+      addImage()
+      addButton()
   }
-
-  @objc func Roll(sender: Any) {
-    let num = Int.random(in: 0..<6)
-    image.rotate()
-    switch num {
-      case 0:
-        image.image = UIImage(named: dice[num])
-      case 1:
-        image.image = UIImage(named: dice[num])
-      case 2:
-        image.image = UIImage(named: dice[num])
-      case 3:
-        image.image = UIImage(named: dice[num])
-      case 4:
-        image.image = UIImage(named: dice[num])
-      case 5:
-        image.image = UIImage(named: dice[num])
-      default:
-        break
+    private func addImage() {
+        image = UIImageView()
+        image.image = UIImage(named: dice[0])
+        view.addSubview(image)
+        image.translatesAutoresizingMaskIntoConstraints = false
+        let constraints = [
+          image.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+          image.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+          image.widthAnchor.constraint(equalToConstant: 200),
+          image.heightAnchor.constraint(equalToConstant: 200)
+        ]
+        NSLayoutConstraint.activate(constraints)
     }
+    private func addButton() {
+        button = UIButton()
+        button.setTitle("Roll", for: .normal)
+        button.setTitleColor(.black, for: .highlighted)
+        button.backgroundColor = .red
+        button.layer.cornerRadius = 20
+        view.addSubview(button)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        let constraintsBTN = [
+          button.topAnchor.constraint(equalTo: image.bottomAnchor, constant: 50),
+          button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+          button.widthAnchor.constraint(equalToConstant: 100),
+          button.heightAnchor.constraint(equalToConstant: 50)
+        ]
+        NSLayoutConstraint.activate(constraintsBTN)
+        button.addTarget(self, action: #selector(roll), for: .touchUpInside)
+    }
+
+  @objc func roll(sender: Any) {
+
+      let rotation = CABasicAnimation(keyPath: "transform.rotation.z")
+      rotation.toValue = NSNumber(value: Double.pi * 2)
+      rotation.duration = 1
+      rotation.isCumulative = true
+      rotation.repeatCount = 1
+      perform(#selector(changeImage), with: nil, afterDelay: 1)
+      image.layer.add(rotation, forKey: nil)
   }
+    @objc func changeImage(sender: Any) {
+        let num = Int.random(in: 0..<6)
+        switch num {
+        case 0:
+            image.image = UIImage(named: dice[num])
+        case 1:
+            image.image = UIImage(named: dice[num])
+        case 2:
+            image.image = UIImage(named: dice[num])
+        case 3:
+            image.image = UIImage(named: dice[num])
+        case 4:
+            image.image = UIImage(named: dice[num])
+        case 5:
+            image.image = UIImage(named: dice[num])
+        default:
+            break
+        }
+    }
 }
-extension UIView {
+extension UIImageView {
   func rotate() {
     let rotation = CABasicAnimation(keyPath: "transfprm.rotation.z")
     rotation.toValue = NSNumber(value: Double.pi * 2)
     rotation.duration = 1
     rotation.isCumulative = true
-    rotation.repeatCount = Float.greatestFiniteMagnitude
+    rotation.repeatCount = 1
     self.layer.add(rotation, forKey: nil)
   }
 }
-
